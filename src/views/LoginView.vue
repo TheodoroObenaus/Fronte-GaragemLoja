f29fb2f5008029eb63153f368af4cf33ab79b727
TheodoroObenaus <theodoro.obenaus@gmail.com>
2024-07-10 16:52:28 -0300

    brabo apenas THREE

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="nP2M7bNqbXH2AkGqRWa4YWE6"
+    ></passage-auth>
+  </div>
 </template>
+
+<style></style>
